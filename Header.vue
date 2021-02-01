0bdfd3715db4a57a5182fe2a8c9036aff6b2741f
sudhir <isudhir.developer@gmail.com>
2021-02-01 09:12:11 +0530

    vue header and sidebar drawer

---
+++ Header.vue
@@ -0,0 +1,143 @@
+<template>
+ 
+    <b-navbar class="s-nav-bg" toggleable="lg" type="dark" variant="primary">
+        <!-- <b-navbar-brand href="#">ALKATECH-CRM</b-navbar-brand> -->
+        <b-navbar-toggle v-b-toggle.sidebar-backdrop target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-button  @click="openNav"   class="btn ripple"  > <i class="fas fa-bars" variant="primary"></i> </b-button>
+        </b-navbar-nav>
+
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+                <em><i class="fa fa-user" /></em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+name:'Sidebar',
+methods:{
+  openNav() {
+  document.getElementById("sidebar-backdrop").setAttribute("style", "width:320px;transition: .5s;")
+  // document.getElementById("app").classList.add("main-app-push-right");
+  document.getElementById("app").setAttribute("style", "margin-left:320px;transition: .5s;")
+}
+}
+}
+
+// function openNav() {
+//   document.getElementById("sidebar-backdrop").classList.add("open-sidenav");
+//   document.getElementById("app").classList.add("main-app-push-right");
+// }
+// openNav();
+// closeNav();
+// function closeNav() {
+//   document.getElementById("sidebar-backdrop").classList.add("close-sidenav");
+//   document.getElementById("app").classList.add("main-app");
+// }
+
+
+
+</script>
+<style>
+ .close-sidenav{
+    width: 0px !important;
+  }
+  .open-sidenav{
+    width: 320px !important;
+  }
+  .main-app-push-right{
+    margin-left: 320px !important;
+  }
+  .main-app{
+    margin-left: 0px !important;
+  }
+
+
+
+.s-nav-bg{
+        background-color: #330a75!important;
+}
+.ripple {
+  position: relative;
+  overflow: hidden;
+  transform: translate3d(0, 0, 0);
+}
+
+.ripple:after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  background-image: radial-gradient(circle, #fff 10%, transparent 10.01%);
+  background-repeat: no-repeat;
+  background-position: 50%;
+  transform: scale(10, 10);
+  opacity: 0;
+  transition: transform .5s, opacity 1s;
+}
+
+.ripple:active:after {
+  transform: scale(0, 0);
+  opacity: .3;
+  transition: 0s;
+}
+
+/* btn styles */
+.btn {
+  background-color: #3e3ebb;
+  color: #fff;
+  font-size: 1.3rem;
+  padding: 6px 12px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  text-decoration: none;
+  transition: 0.3s;
+  border: 2px solid #3e3ebb;
+  outline: none;
+  white-space: nowrap;
+  font-weight: 500;
+  vertical-align: middle;
+  margin: 0px;
+  box-shadow: 0 0 2px -2px rgba(29, 39, 231, .1),
+    0 0 3px 0 rgba(29, 39, 231, .1),
+    0 0 5px 0 rgba(29, 39, 231, .1),
+    0 2px 2px -4px rgba(29, 39, 231, .1),
+    0 4px 8px 0 rgba(29, 39, 231, .1),
+    0 2px 15px 0 rgba(29, 39, 231, .1)
+}
+.btn:hover {
+  background-color: #131BB4;
+  border-color: #131BB4;
+  color: #fff;
+}
+  .button.btn.ripple.btn-secondary:focus{
+    background: #1d27e7;
+  }
+</style>
---
+++ Sidebar.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-backdrop" title="" >
+        <template #default="{hide}">
+          <b-link @click="closeNav"><i class="btn fa fa-close fa-times"/></b-link>
+              <b-navbar-brand href="#">    
+                <img alt="alkatech logo" src="./../../../assets/alkatech.png">
+              </b-navbar-brand>
+              
+            <div class="px-3 py-2">
+              <!-- <b-form-group label="Backdrop variant" label-for="">
+                <b-form-select id="backdrop-variant" v-model="variant" :options="variants"></b-form-select>
+              </b-form-group> -->
+                <nav class="mb-3 s-sidenav-text">
+                  <hr>
+                  <b-nav vertical>
+                 
+                    
+                    <b-nav-item to="link1" @click="hide"><i class="fa fa-address-card" /> &nbsp; &nbsp; Link1 </b-nav-item>
+                    <b-nav-item to="link2" @click="hide"><i class="fa fa-address-card" /> &nbsp; &nbsp; Link2 </b-nav-item>
+                    <b-nav-item to="link3" @click="hide"><i class="fa fa-address-card" /> &nbsp; &nbsp; Link3 </b-nav-item>
+
+                  </b-nav>
+                </nav>
+              
+            </div>
+        </template >
+    </b-sidebar> 
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'Sidebar',
+    data() {
+      return {
+        variant: 'dark',
+        variants: [
+          'transparent',
+          'white',
+          'light',
+          'dark',
+          'primary',
+          'secondary',
+          'success',
+          'danger',
+          'warning',
+          'info',
+        ]
+      }
+    },
+    methods:{
+  closeNav() {
+  document.getElementById("sidebar-backdrop").setAttribute("style", "width:0px; transition: .5s;")
+  // document.getElementById("app").classList.add("main-app-push-right");
+  document.getElementById("app").setAttribute("style", "margin-left:0px; transition: .5s;")
+}
+}
+  }
+
+  
+
+</script>
+<style >
+.s-sidenav-text{
+  text-align: left;
+}
+
+  .s-fix-bottom-pposition{
+    position: fixed;
+    bottom:0;
+    left:0;
+  }
+ .close{
+   display: none !important;
+ }
+ .fa-close{  position: absolute;
+    top: 6px;
+    right: 6px;
+    font-size: 20px;
+    /* border: 2px solid blue; */
+    padding: 6px;
+    height: 32px;
+    background: black;
+    width: 32px;
+    border-radius: 100%;
+    box-shadow: -1px 1px 8px 0px red;
+}
+</style>
